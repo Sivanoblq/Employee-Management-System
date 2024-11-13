55066f2eb61c4a2c0dce9e9cb50c4350afdc499c
Sivakumar Arjunan <sivakumar.a@noblq.com>
2024-11-13 11:25:40 +0530

    Updated

--- Program.cs
+++ Program.cs
@@ -19,7 +19,13 @@ using Employee_System.Validator;
 using FluentValidation.AspNetCore;
 using Npgsql.EntityFrameworkCore.PostgreSQL;
 using System.Text.Json.Serialization;
+using NLog;
+using NLog.Web;
 
+var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+try
+{
+    logger.Info("Application is starting up");
 var builder = WebApplication.CreateBuilder(args);
 
 //builder.Services.AddControllers().AddJsonOptions(options =>
@@ -30,7 +36,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Register repositories and services
 builder.Services.AddScoped<IGenericRepository<Employee>, EmployeeRepository>();
@@ -153,9 +159,9 @@ async Task SeedRolesAsync(RoleManager<IdentityRole> roleManager)
     // Define roles with custom Ids
     var roles = new List<IdentityRole>
     {
-        new IdentityRole { Id = "1", Name = "Admin", NormalizedName = "ADMIN" },
-        new IdentityRole { Id = "2", Name = "HR", NormalizedName = "HR" },
-        new IdentityRole { Id = "3", Name = "Employee", NormalizedName = "EMPLOYEE" }
+        new IdentityRole { Id = "Admin", Name = "Admin", NormalizedName = "ADMIN" },
+        new IdentityRole { Id = "HR", Name = "HR", NormalizedName = "HR" },
+        new IdentityRole { Id = "Employee", Name = "Employee", NormalizedName = "EMPLOYEE" }
     };
 
     foreach (var role in roles)
@@ -179,3 +185,13 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers(); // This line remains unchanged
 app.Run();
+}
+catch (Exception ex)
+{
+    logger.Error(ex, "Application stopped due to an exception");
+    throw;
+}
+finally
+{
+    LogManager.Shutdown();
+}
